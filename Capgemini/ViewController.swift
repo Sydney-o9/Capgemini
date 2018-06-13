90603f01e61c0ed75e1123a0e97a4c8b6705af30
Anthony Rey <anthony.rey21@gmail.com>
2018-06-13 16:43:46 +1000

    feat(Link Model to View)
    - [x] Connected DataSource & Delegate from storyboard to ViewController
    - [x] Added UITableViewDataSource, UITableViewDelegate methods to
    handle rows
    
    Work on #8

--- Capgemini/ViewController.swift
+++ Capgemini/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK:  - IBOutlets
+    
+    @IBOutlet var tableView: UITableView!
+    
     // MARK:  - Properties
     
     var feedDownloader: FeedDownloader?
+    
+    var tableData: [Story] = []
 
     // MARK:  - UIViewController
     
@@ -20,6 +26,9 @@ class ViewController: UIViewController {
      */
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.rowHeight = 80
+        tableView.estimatedRowHeight = UITableViewAutomaticDimension
         
         feedDownloader = FeedDownloader(feedURL: CapgeminiApi.feedURL)
         feedDownloader?.delegate = self
@@ -50,7 +59,48 @@ extension ViewController: FeedDownloaderDelegate {
         if let title = feedDownloader?.decodedFeed?.title {
             self.title = title
         }
+        
+        if let stories = feedDownloader?.decodedFeed?.stories {
+            self.tableData = stories.filter { $0.title?.isEmpty == false }
+            self.tableView.reloadData()
+        }
 
     }
+}
+
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        print("Dequeing cell \(indexPath.row)")
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StoryTableViewCellIdentifier", for: indexPath) as! StoryTableViewCell
+        let story: Story = self.tableData[indexPath.row]
+        
+        if let title = story.title {
+            cell.storyTitleLabel?.text = title
+        }
+        
+        if let description = story.desc {
+            cell.storyDescriptionLabel?.textColor = UIColor.black
+            cell.storyDescriptionLabel?.text = description
+        } else {
+            cell.storyDescriptionLabel?.textColor = UIColor.gray
+            cell.storyDescriptionLabel?.text = "No description for this story"
+        }
+        
+        return cell
+    }
+    
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
 }
 
